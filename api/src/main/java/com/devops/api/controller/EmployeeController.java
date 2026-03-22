c9d6a59f28a12d2ca15e33f8b617dccb9b4c1a36
GBGodea <gordeyka2003@mail.ru>
2026-03-22 22:50:22 +0300

    update cors

--- api/src/main/java/com/devops/api/controller/EmployeeController.java
+++ api/src/main/java/com/devops/api/controller/EmployeeController.java
@@ -14,8 +14,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = {
-        "http://localhost:5173",
-        "http://localhost:3000"
+        "*"
 })
 public class EmployeeController {
     private final EmployeeRepository repository;
